447b7edcf761e7cd61f932ea96157d81a45ebb76
Manuele Longo <mlongo@e3r4p6.42roma.it>
2023-07-21 17:27:21 +0200

    ex00 sub04

---
+++ Subject04/ex00/ImagesToChar/src/java/edu.Roma42.printer/logic/Program.java
@@ -0,0 +1,41 @@
+package edu.Roma42.printer.logic;
+import  edu.Roma42.printer.app.Img;
+
+import java.util.*;
+import java.io.*;
+import java.lang.Object;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import javax.swing.*;
+
+
+public class Program {
+	public static void main(String[] args) {
+
+		if (args.length != 3) {
+			System.out.println("Input wrong");
+			return ;
+		}
+
+		if (args[0].length() != 1 || args[1].length() != 1) {
+			System.out.println("Input wrong");
+			return ;
+		}
+
+		Img imgbmp = new Img();
+
+		try {
+			imgbmp.seeBMPImage(args[2], args[0].toCharArray()[0], args[1].toCharArray()[0]);
+		}
+
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
---
+++ Subject04/ex00/README.txt
@@ -0,0 +1,16 @@
+PROJECT DESCRIPTION:
+
+this project has to convert a bmp two-colored image (only black and white) in a rappresentation of its own on the standard output where an ASCII character will rappresent
+black pixels and another one white pixels, the character for white pixel and the character for the black pixel are get from the input parameters and the path of the file
+aswell in particular the form of the the excecution will be explained.
+
+COMPILATION:
+
+javac --release 8 -d ./target ./src/java/edu.Roma42.printer/*/*.java
+
+EXCECUTION:
+
+java -cp ./target edu.Roma42.printer.logic.Program <black_char> <white_char> <path_bmp>
+
+
+
