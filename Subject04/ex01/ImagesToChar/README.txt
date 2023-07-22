5482a887cb0a83f309cac952710a93c69b67e2ce
Manuele Longo <mlongo@e3r4p4.42roma.it>
2023-07-22 12:54:53 +0200

    Sub04 in progress

---
+++ Subject04/ex01/ImagesToChar/README.txt
@@ -0,0 +1,26 @@
+PROJECT DESCRIPTION:
+
+this project has to convert a bmp two-colored image (only black and white) in a rappresentation of its own on the standard output where an ASCII character will rappresent
+black pixels and another one white pixels, the character for white pixel and the character for the black pixel are get from the input parameters and the path of the file
+aswell in particular the form of the the excecution will be explained.
+
+COMPILATION:
+
+javac --release 8 -d ./target ./src/java/edu.Roma42.printer/*/*.java
+
+EXCECUTION:
+
+cp -r src/resources target
+java -cp ./target edu.Roma42.printer.logic.Program <black_char> <white_char> <path_bmp>
+
+JAR ASSEMBLY:
+
+javac --release 8 -d ./target ./src/java/edu.Roma42.printer/*/*.java
+cp -r src/resources target
+jar -cfmv target/images-to-chars-printer.jar ./src/manifest.txt -C target edu -C . target/resources
+
+JAR STARTUP:
+
+java -jar target/images-to-chars-printer.jar <black_char> <white_char>
+
+
---
+++ Subject04/ex01/ImagesToChar/src/java/edu.Roma42.printer/app/Img.java
@@ -0,0 +1,47 @@
+package edu.Roma42.printer.app;
+import edu.Roma42.printer.logic.*;
+
+import java.util.*;
+import java.io.*;
+import java.lang.Object;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import javax.swing.*;
+
+public class Img {
+
+	public Img() {
+	}
+
+	public void seeBMPImage(String BMPFileName, char black, char white) throws IOException {
+		BufferedImage image = ImageIO.read(new FileInputStream(BMPFileName));
+
+		int[][] array2D = new int[image.getHeight()][image.getWidth()]; //*
+
+		for (int xPixel = 0; xPixel < image.getHeight(); xPixel++) //*
+		{
+			for (int yPixel = 0; yPixel < image.getWidth(); yPixel++) //*
+			{
+				int color = image.getRGB(yPixel, xPixel); //*
+				if (color==Color.BLACK.getRGB()) {
+					array2D[xPixel][yPixel] = black;
+				} else {
+					array2D[xPixel][yPixel] = white; // ?
+				}
+			}
+		}
+
+		for (int x = 0; x < array2D.length; x++)
+		{
+			for (int y = 0; y < array2D[x].length; y++)
+			{
+				System.out.print((char)array2D[x][y]);
+			}
+			System.out.println();
+		}
+	}
+}
