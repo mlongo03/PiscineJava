e3dceae01b02ca09a95f71d2df5a25c8de1624b0
Manuele longo <126604785+mlongo03@users.noreply.github.com>
2023-08-14 12:53:52 +0200

    Delete ChatroomRepository 2.java

--- Subject05/ex02/Chat/src/main/java/edu.Roma42.chat/repositories/ChatroomRepository 2.java
+++
@@ -1,9 +0,0 @@
-package edu.Roma42.chat.repositories;
-
-import edu.Roma42.chat.models.Chatroom;
-import java.util.*;
-
-public interface ChatroomRepository {
-
-	Optional<Chatroom> findById(Long id);
-}
